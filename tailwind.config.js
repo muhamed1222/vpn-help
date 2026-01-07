30497347dd627b3219793464f4c8931be1a0af14
outcastsdev-svg <outcastsdev@gmail.com>
2026-01-07 16:07:47 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './theme.config.tsx',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['GT Walsheim Pro', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
